1ca5333370afb42b9c1d4691585c5bda49cab8d4
cavalancian <ncavalancia@gmail.com>
2018-02-15 12:51:02 -0500

    Added items to gridBagLayout
    2/15/18

--- Prog3/src/Program3.java
+++ Prog3/src/Program3.java
@@ -5,18 +5,28 @@ import java.awt.event.*;
 import java.lang.*;
 
 
-public class Program3 extends Frame implements WindowListener, ActionListener{
-    Label message = new Label("It worked!");
-	List list = new List(13);
+
+public class Program3 
+	extends Frame 
+	implements WindowListener, ActionListener {
+    //lists
+	List directoryList = new List(13);
+	//labels
+	Label srcLabel = new Label("Source");
+	Label srcFileLabel = new Label();
+	Label tgtDirLabel = new Label();
+	Label sysMsgLabel = new Label();
+	//buttons
+	Button okBtn = new Button("Ok");
+	Button tgtBtn = new Button("Target");
+	//text fields
+	TextField tgtFileName = new TextField("Text field........",60);
 	
-	Label scrFileLabel = new Label();
-	int mode = 0;
-	File curdir;
     public Program3 () {
-        double colWeights[] = {1.0,1.0,1.0,1.0};
-        double rowWeights[] = {1.0,1.0,1.0,1.0};
-        int rowHeight[] = {1,1,1,1};
-        int colWidth[] = {1,1,1,1};
+        double colWeights[] = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
+        double rowWeights[] = {1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0};
+        int rowHeight[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        int colWidth[] = {1,1,1,1,1,1,1,1};
         GridBagConstraints c = new GridBagConstraints();
         GridBagLayout lm = new GridBagLayout();
         lm.columnWeights = colWeights;
@@ -25,58 +35,126 @@ public class Program3 extends Frame implements WindowListener, ActionListener{
         lm.rowHeights = rowHeight;
         c.anchor = GridBagConstraints.EAST;
         this.setLayout(lm);
-
+		
+		//add directory listing
         c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 8;
+        c.gridheight = 13;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 0;
+		lm.setConstraints(directoryList, c);
+		directoryList.addActionListener(this);
+        this.add(directoryList);
+		
+		//add src label
+		c.weightx = 1.0;
         c.weighty = 1.0;
         c.gridwidth = 1;
         c.gridheight = 1;
         c.fill = GridBagConstraints.BOTH;
-        c.gridx = 3;
-        c.gridy = 3;
-
-        lm.setConstraints(message, c);
-        this.add(message);
+        c.gridx = 0;
+        c.gridy = 13;
+		lm.setConstraints(srcLabel, c);
+        this.add(srcLabel);
 		
+		//add source file label
 		c.weightx = 1.0;
-		c.weighty = 1.0;
+        c.weighty = 1.0;
         c.gridwidth = 7;
         c.gridheight = 1;
         c.fill = GridBagConstraints.BOTH;
-        c.gridx = 0;
-        c.gridy = 3;
-		lm.setConstraints(scrFileLabel, c);
-		this.add(scrFileLabel);
+        c.gridx = 1;
+        c.gridy = 13;
+		lm.setConstraints(srcFileLabel, c);
+        this.add(srcFileLabel);
+		srcFileLabel.setText("srcFileLabel");
 		
-		this.setLayout(lm);
-		this.add(list);
-  		
+		//add target directory label
+		c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 7;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 1;
+        c.gridy = 14;
+		lm.setConstraints(tgtDirLabel, c);
+        this.add(tgtDirLabel);
+		tgtDirLabel.setText("tgtDirLabel");
+		
+		//add system message label
+		c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 6;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 1;
+        c.gridy = 16;
+		lm.setConstraints(sysMsgLabel, c);
+        this.add(sysMsgLabel);
+		sysMsgLabel.setText("sysMsgLabel");
+		
+		//add ok button
+		c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 7;
+        c.gridy = 15;
+		lm.setConstraints(okBtn, c);
+        this.add(okBtn);
+		
+		//add target button
+		c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 1;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.gridy = 14;
+		lm.setConstraints(tgtBtn, c);
+        this.add(tgtBtn);
+		
+		//add target filename field
+		c.weightx = 1.0;
+        c.weighty = 1.0;
+        c.gridwidth = 6;
+        c.gridheight = 1;
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 1;
+        c.gridy = 12;
+		lm.setConstraints(tgtFileName, c);
+		Color clr = Color.black;
+		tgtFileName.setBackground(clr);
+        this.add(tgtFileName);
+		
         this.setResizable(true);
         this.setVisible(true);
         this.addWindowListener(this);
 		this.setTitle("My window");
-		list.addActionListener(this);
-		
-		
         this.pack();
 		
-		File g = new File("");
-		String s = g.getAbsolutePath();
-		curdir = new File(s); 
-		display(null);
+		display();
 
     } //end of constructor
-	
-	
+
     public static void main (String[] args) {
         Program3 window = new Program3();
         window.setBounds(20,20,600,600);
-		File curdir = new File("."); 
+		File f = new File("");
+		String s = f.getAbsolutePath();
+		f = new File(s);
+		System.out.println(f.getParent());
 		
-		//System.out.println("Parent= " + curdir.getParent());
-
 		
-		//window.add(list);
     }
+	
+	public void display() {
+		
+		this.setTitle("Hello");
+	}
 
     public void stop () {
         this.removeWindowListener(this);
@@ -112,123 +190,18 @@ public class Program3 extends Frame implements WindowListener, ActionListener{
         System.out.println("Window maximized");
     }
 	
-	public void display(String name){  //name is the item click in the list
-	
-		switch(mode)
-		{
-		
-			case 0:  //Source Mode
-			/*
-			Click in List
-			Set Source
-			Adds +
-			Target and Copy/OK buttons are disabled
-			File name text field disabled
-			When Target button pressed go to mode 1			
-			*/
-			
-			//File g = new File("");
-			//String s = g.getAbsolutePath();
-			//curdir = new File(s); 
-		
-			//System.out.println("Parent= " + curdir.getParent());
-			//window.setTitle(curdir.getAbsolutePath());
-			//List list = new List(13);
-			if(name == null)
-			{
-				
-				
-			}
-			else 
-			{
-				if( name.equals(".."))
-				{
-					curdir = new File(curdir.getParent());
-					System.out.println("Parent= " + curdir.getParent());
-				}
-				else
-				{
-					File f = new File(curdir, name);
-					if(f.isDirectory())
-					{
-						curdir = new File(curdir, name);
-						System.out.println("Directory: " + curdir.getAbsolutePath());
-						
-					}
-					else
-					{
-						//check mode
-						//set appropirate file name if mode == 0 set srcfilelabel to curdir
-						scrFileLabel.setText(curdir.getAbsolutePath() + "\\" + name);
-					}
-				}
-			}
-			
-			
-			String[] filenames;
-			filenames = curdir.list();
-			
-			if(filenames == null)
-			{
-				filenames = new String[]{};
-				System.out.println("Empty Array");
-			}
-			else
-			{
-				this.setTitle(curdir.getAbsolutePath());
-			}
-			list.removeAll();
-			if(curdir.getParent() != null)
-			{
-				list.add("..");
-				
-			
-			}
-			for(int i=0; i< filenames.length; i++)
-			{
-				list.add(filenames[i]);
-			
-			
-			}
-			break;
-			
-			case 1:  //Target/Copy Mode
-			/*
-			Click on list
-			Set Target
-			Adds +
-			Target button pressed add path to target label
-			Can input into file name field
-			Ok button or Enter moves to mode 2
-			
-			
-			*/
-			break;
-			
-			
-			case 2: //Copy mode
-			/*
-			Needs valid inout and output
-			copies files
-			may overwrite current file
-			if same filename is moved to different directory it creates a new file with that name in the new directory
-			*/
-			break;
-		
-			
+	public void actionPerformed (ActionEvent e) {
+		/*Object s = e.getSource();
+		if (s == list) {
+			System.out.println("You clicked in the list!!!");
 		}
-	}
-		public void actionPerformed(ActionEvent e) {
-		Object s = e.getSource();
-		if(s == list)
-		{
-			String item = list.getSelectedItem();
-			//might have to remove the +
-			System.out.println(item + " is selected");
-			display(item);
-			
+		if (s == button) {
+			System.out.println("You clicked the button!!!");
 		}
-	}
+		if (s == message) {
+			System.out.println("You clicked in the list!!!");
+		}
+	*/}
+
 }
-	
 
